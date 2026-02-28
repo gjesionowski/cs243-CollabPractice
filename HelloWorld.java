83537a9231ee35774cffbc354dca7a29556ab7e0
Gori <36163958+gjesionowski@users.noreply.github.com>
2026-02-27 23:14:20 -0800

    Update comment to reflect conflict resolution

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,7 +2,7 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
         System.out.println("Welcome to Java development.");
-        // This is a comment for conflict
+        // This is a comment of conflict
         System.out.println("Welcome to Java programming!");
     }
 }
