9a60e436420e82fc2b2e21ea7b96f02af6839268
Gori <36163958+gjesionowski@users.noreply.github.com>
2026-02-27 23:17:47 -0800

    Modify welcome messages in HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,8 +1,8 @@
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
-        System.out.println("Welcome to Java development.");
+        System.out.println("Walrus to Java development.");
         // This is a conflict
-        System.out.println("Welcome to Java programming!");
+        System.out.println("Whale to Java programming!");
     }
 }
