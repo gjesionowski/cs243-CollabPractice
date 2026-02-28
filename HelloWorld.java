78b52738785605c44a8cbad1f13f94d176e736db
gjesionowski <36163958+gjesionowski@users.noreply.github.com>
2026-02-27 23:18:12 -0800

    Test

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,8 +1,8 @@
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
-        System.out.println("Welcome to Java development.");
-        // This is a conflict
-        System.out.println("Welcome to Java programming!");
+        System.out.println("Wilcome to Java development.");
+        // This is now a conflict
+        System.out.println("Walcome to Java programming!");
     }
 }
